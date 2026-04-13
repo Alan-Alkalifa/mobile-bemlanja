e7499f620a30e77b1a48facb941fb3f6910aa36c
Alan-Alkalifa <work.alanalkalifa@gmail.com>
2026-04-14 01:50:40 +0700

    update

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,8 +1,8 @@
-import "../globals.css";
 import { Stack } from "expo-router";
 import { AuthProvider } from "../contexts/AuthProvider";
 import { CartProvider } from "../contexts/CartProvider";
 import { CartSelectionProvider } from "../contexts/CartSelectionProvider";
+import "../globals.css";
 
 export default function RootLayout() {
   return (
